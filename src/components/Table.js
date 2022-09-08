5a90bf52dd5d6aa3f10dfbe1a095833c23e20a61
Leonardo <Leonardo.Yakub@gmail.com>
2022-09-08 19:03:19 +0200

    adding functionality for changing rpp

--- src/components/Table.js
+++ src/components/Table.js
@@ -8,6 +8,7 @@ import Filter from './Filter';
 
 const Table = () => {
 	const [vehicles, setVehicles] = useState();
+	const [rpp, setRpp] = useState(10);
 	const [page, setPage] = useState(1);
 	const [totalPages, setTotalPages] = useState();
 	const [displayFilterBtns, setDisplayFilterBtns] = useState(false);
@@ -16,6 +17,7 @@ const Table = () => {
 	const [searchQuery, setSearchQuery] = useState();
 	const edit = ["Make", "Name", "Year", "Price"];
 	const filterEdit = [edit[0], ...edit.slice(2)];
+	const rppList = [10, 25, 50, 100]
 
  	useEffect(() => {
 		const cancelRequest = axios.CancelToken.source();
@@ -24,7 +26,7 @@ const Table = () => {
 			method: "get",
 			url: "https://api.baasic.com/beta/simple-vehicle-app/resources/VehicleModel",
 			params: {
-				rpp: 10,
+				rpp: rpp,
 				page: page,
 				sort: sort.value && `${sort.value}|${order.direction}`,
 				searchQuery: 
@@ -44,11 +46,11 @@ const Table = () => {
 		.catch(err => console.error(err));
 
 		return () => cancelRequest.cancel();
-	}, [page, sort.value, order.direction, filterQuery, searchQuery]);
+	}, [rpp, page, sort.value, order.direction, filterQuery, searchQuery]);
 
 	useEffect(() => {
 		setPage(1);
-	}, [filterQuery, searchQuery]);
+	}, [filterQuery, searchQuery, totalPages]);
 
 	const closeFilterBtn = () => {
 		setDisplayFilterBtns(prev => !prev);
@@ -63,12 +65,18 @@ const Table = () => {
 		setSearchQuery(`where name like '%${e.currentTarget.value.trim().replace(/\s+/g, '-')}%' or make like '%${e.currentTarget.value.trim().replace(/\s+/g, '-')}%' or year like '%${e.currentTarget.value.trim().replace(/\s+/g, '-')}%' or price like '%${e.currentTarget.value.trim().replace(/\s+/g, '-')}%'`);
 	}
 
-	const styling = (name) => {
+	const filterStyling = (name) => {
 		return {
 			border: `${name && !brightness.darkMode ? '3px solid #ff008030' : name && brightness.darkMode ? '3px solid #8983f7' : ''}`,
 			color: `${name && !brightness.darkMode ? '#ff008030' : name && brightness.darkMode ? '#8983f7' : ''}`,
 			fontWeight: `${name ? '600' : ''}`,
 			transition: `${name ? 'unset' : ''}`
+		}
+	}
+
+	const rppStyling = (name) => {
+		return {
+			opacity: parseInt(name) === rpp ? '.4' : ''
 		}
 	}
 	return (
@@ -77,7 +85,7 @@ const Table = () => {
 				<div>
 					<button 
 						onClick={closeFilterBtn}
-						style={styling(displayFilterBtns)} 
+						style={filterStyling(displayFilterBtns)} 
 					>
 						Filter
 					</button>
@@ -100,7 +108,7 @@ const Table = () => {
 										}
 									}
 								}
-								style={styling(elem === filterCategory)}
+								style={filterStyling(elem === filterCategory)}
 							>
 								{elem}
 							</button>
@@ -114,28 +122,42 @@ const Table = () => {
 					/>
 				</div>
 			</div>
-			
-			<input 
-				id='search_input' 
-				className={!brightness.darkMode ? 'sun_color_full_border' : 'moon_color_full_border'} 
-				type="text" 
-				placeholder='Search' 
-				onInput={updatingSearchQuery} 
-			/>
 
-			<div id='thead' className={!brightness.darkMode ? 'sun_color_bg' : 'moon_color_bg'}>
-				{edit.map((elem, index) => <TableHead key={index} name={elem}/>)}
-			</div>
-
-			{vehicles && vehicles.map(elem => (
-				<div key={elem.id} className={`list ${!brightness.darkMode ? 'sun_color_border sun_color_hover' : 'moon_color_border moon_color_hover'}`}>
-					<p>{elem.make.replace(/-+/g, ' ')}</p>
-					<p>{elem.name.replace(/-+/g, ' ')}</p>
-					<p>{elem.year}</p>
-					<p>{`$${elem.price.toLocaleString('en-US')}`}</p>
-					<p><Link to={`${elem.id}`}>details</Link></p>
+			<div id='search'>
+				<input 
+					id='search_input' 
+					className={!brightness.darkMode ? 'sun_color_full_border' : 'moon_color_full_border'} 
+					type="text" 
+					placeholder='Search' 
+					onInput={updatingSearchQuery} 
+				/>
+				<div id='rpp' className={!brightness.darkMode ? 'sun_color_full_border' : 'moon_color_full_border'}>
+					{rppList.map((elem, index) => 
+						<p 
+							key={index} 
+							onClick={() => setRpp(parseInt(elem))} 
+							style={rppStyling(elem)}
+						>
+							{elem}
+						</p>)}
 				</div>
-			))}
+			</div>
+			
+			<div id='table'>
+				<div id='thead' className={!brightness.darkMode ? 'sun_color_bg' : 'moon_color_bg'}>
+					{edit.map((elem, index) => <TableHead key={index} name={elem}/>)}
+				</div>
+
+				{vehicles && vehicles.map(elem => (
+					<div key={elem.id} className={`list ${!brightness.darkMode ? 'sun_color_border sun_color_hover' : 'moon_color_border moon_color_hover'}`}>
+						<p>{elem.make.replace(/-+/g, ' ')}</p>
+						<p>{elem.name.replace(/-+/g, ' ')}</p>
+						<p>{elem.year}</p>
+						<p>{`$${Intl.NumberFormat('en', {notation: 'compact'}).format(elem.price)}`}</p>
+						<p><Link to={`${elem.id}`}>details</Link></p>
+					</div>
+				))}
+			</div>
 
 			<div id='paging'>
 				<button 
